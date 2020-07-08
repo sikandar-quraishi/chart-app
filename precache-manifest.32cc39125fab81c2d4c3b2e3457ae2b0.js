4dee94dd30bdc354902801078a8ba51b11dbb2a2
zuaam <nriittech@gmail.com>
2020-07-08 19:53:43 +0530

    Updates

---
+++ precache-manifest.32cc39125fab81c2d4c3b2e3457ae2b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d2315aff6611e4d5ca2d9417306f342",
+    "url": "/chart-app/index.html"
+  },
+  {
+    "revision": "189769dc12d894bf69bd",
+    "url": "/chart-app/static/css/main.2d4d78a4.chunk.css"
+  },
+  {
+    "revision": "037f020c18dedf8f49cb",
+    "url": "/chart-app/static/js/2.826e2219.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/chart-app/static/js/2.826e2219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "189769dc12d894bf69bd",
+    "url": "/chart-app/static/js/main.48bc64f6.chunk.js"
+  },
+  {
+    "revision": "c515bba2ccdd7aea6d89",
+    "url": "/chart-app/static/js/runtime-main.e775e2a4.js"
+  }
+]);
